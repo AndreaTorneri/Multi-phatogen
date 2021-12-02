2af8752c615c6434e0d18965bab30df07e8f6b0d
ATorneri <48247730+AndreaTorneri@users.noreply.github.com>
2021-12-02 16:00:32 +0100

    Add files via upload

---
+++ R_comp_netw.R
@@ -0,0 +1,138 @@
+# Function to compute the value of the reproduction number in a 2-level household mixing. 
+#Constant infectious period
+
+
+
+
+
+R0.computation.StaticNetw<-function(HH.network,beta.g,nSim, beta.h){
+  mu<-1
+  hh.id<-HH.network %v% "hh_id"
+  hh.size<-HH.network %v% "hh_size"
+  m<- length(unique(hh.id)) # number of households
+  m.n<-rep(0,max(unique(hh.size))) # number of household of a specific size (position 1 size 1, position 2 size 2, ....)
+  R0<-0
+  for (i in 1:length(m.n)){
+  m.n[i]<-length(unique(hh.id[which(hh.size==i)]))
+}  
+  h.n<-m.n/m #proportion of household of a specific size
+  mu.h<-sum(h.n*(1:length(h.n)))
+  
+    
+  #divide network in sizes
+  for (j in 1:nSim){
+    AR<-list()
+    for (s in 1:max(unique(hh.size))) {
+      AR[[s]]<-0  
+    }
+      for (w in unique(hh.id)){
+        #print(w)
+        hh.data<-data.frame("members"= which(hh.id==w),"id"=1:length(which(hh.id==w)),"status"=0,"recovery"=Inf, "index.contact"=0)
+        #index case
+        primary<-sample(1:length(hh.data$members),1)
+        hh.data$status[primary]<-1 
+        hh.data$recovery[primary]<-mu
+        hh.data$index.contact[primary]<-1
+        contact.time<-data.frame("id"=hh.data$members,"pr.ctc"=rep(NA,length(hh.data$members)),"pr.infectee"=rep(NA,length(hh.data$members)))   #matrix containing the proposed time of the next possible infectious contact (first colum) 
+        current.time<-0
+        while ((length(which(hh.data$status==1))>0)) { # till there is at least one infectious individual
+          for (i in which(hh.data$index.contact==1) ){ # for all the individuals that has to propose a new contact
+            temp.contact.time<-rexp(1,beta.h)+current.time
+            hh.members.contacted<-get.neighborhood(HH.network,hh.data$members[i], type = "out")
+            hh.data$index.contact[i]<-0
+              ifelse(length(hh.members.contacted)>0,contact.time$pr.infectee[i] <-sample(hh.members.contacted,1),contact.time$pr.infectee[i] <-NA)
+              if (length(hh.members.contacted)==1){contact.time$pr.infectee[i]<-hh.members.contacted}
+              ifelse(length(hh.members.contacted)>0,contact.time$pr.ctc[i]<-temp.contact.time ,contact.time$pr.ctc[i]<-NA)
+            }
+          #computation of the next event
+          ifelse(length(which(is.na(contact.time$pr.ctc)==FALSE))>0,T_h<-min(contact.time$pr.ctc, na.rm = T),T_h<-Inf)
+          R_a<-min(hh.data$recovery, na.rm = T)
+          
+          #next event is an infection
+          if (T_h<R_a){
+            # a household contact
+              current.time<-T_h
+              infector<-which(contact.time$pr.ctc ==T_h)
+              infectee<-hh.data$id[which(hh.data$members==contact.time$pr.infectee[infector])]
+              if (hh.data$status[infectee]==0 ){
+                hh.data$status[infectee]<-1
+                hh.data$recovery[infectee]<-current.time+mu
+                hh.data$index.contact[infectee]<-1
+                hh.data$index.contact[infector]<-1
+                contact.time$pr.ctc[infector]<-NA
+              }else{
+              hh.data$index.contact[infector]<-1
+              contact.time[infector,2:3]<-NA
+            }
+            #Phase 2.3 a recovery occurs  
+          }else{
+            current.time<-R_a
+            recovered<-which(hh.data$recovery==R_a)
+            hh.data$recovery[recovered]<-Inf
+            hh.data$status[recovered]<--1
+            contact.time[recovered,2:3]<-rep(NA,2)
+          }
+          }
+        AR[[hh.size[hh.data$members[1]]]]<-c(AR[[hh.size[hh.data$members[1]]]],length(which(hh.data$status==-1)))
+        # sar - NA when no symptomatic infection are register
+        }
+    
+    ar<-0
+    for (s in 1:max(unique(hh.size))){
+    ar[s]<-ifelse(length(AR[[s]])>1,mean(AR[[s]][-1]),0) 
+    }
+    
+  R0[j]<-beta.g*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
+    print(j)
+  }
+  return(R0)
+}
+
+R0.comp<-function(ratio_hhgl,tol,R.rif,HH.network,nSim){
+  beta.g<-1
+  beta.h<-ratio_hhgl*beta.g
+  beta.g.tempm<-0
+  beta.g.tempM<-10
+  R.temp<-R0.computation.StaticNetw(HH.network = HH.network, beta.g = beta.g, beta.h = beta.h, nSim = nSim)
+  while (abs(mean(R.temp)-R.rif)>tol){
+    R.temp<-R0.computation.StaticNetw(HH.network = HH.network, beta.g = beta.g, beta.h = beta.h, nSim = nSim)
+    if (mean(R.temp)>R.rif){
+      beta.g.tempM<-beta.g
+      beta.g<-runif(1,min = beta.g.tempm,max = beta.g)
+      beta.h<-beta.g*ratio_hhgl
+    }else{
+      beta.g.tempm<-beta.g
+      beta.g<-runif(1,min = beta.g, max = beta.g.tempM)
+      beta.h<-beta.g*ratio_hhgl
+    }
+    print(c(R.rif,mean(R.temp),abs(mean(R.temp)-R.rif)))
+  }
+  transm.prms<-data.frame("beta.g"=beta.g, "beta.h"=beta.h)
+  return(transm.prms)
+}
+
+
+
+
+R0.comp.asy<-function(ratio_hhgl,tol,R.rif,HH.network,nSim,IPL){
+  beta.g<-1
+  beta.h<-ratio_hhgl*beta.g
+  beta.g.tempm<-0
+  beta.g.tempM<-10
+  R.temp<-R0.computation.StaticNetw(HH.network = HH.network, beta.g = beta.g, beta.h = beta.h, nSim = nSim)
+  while (abs(mean(R.temp)-R.rif)>tol){
+    R.temp<-R0.computation.StaticNetw(HH.network = HH.network, beta.g = beta.g, beta.h = beta.h, nSim = nSim)
+    if (mean(R.temp)>R.rif){
+      beta.g.tempM<-beta.g
+      beta.g<-runif(1,min = beta.g.tempm,max = beta.g)
+      beta.h<-beta.g*ratio_hhgl
+    }else{
+      beta.g.tempm<-beta.g
+      beta.g<-runif(1,min = beta.g, max = beta.g.tempM)
+      beta.h<-beta.g*ratio_hhgl
+    }
+    print(c(R.rif,mean(R.temp),abs(mean(R.temp)-R.rif)))
+  }
+  transm.prms<-data.frame("beta.g"=beta.g, "beta.h"=beta.h)
+  return(transm.prms)
+}
---
+++ main.coinfection.new.R
@@ -0,0 +1,106 @@
+#Input parameters from csv
+args <- commandArgs(trailingOnly = TRUE)
+out = args[1] #working directory
+cat(",out=",out)
+cores = as.numeric(args[2]) #number of cores to run in parallel
+cat(",cores=",cores)
+t2 = as.numeric(args[3]) #time at which pathogen 2 is insert in the population
+cat(",t2=",t2)
+sigma12 = as.numeric(args[4]) # short-term interaction parameter: acquiring 2 while having 1 (this value can)
+cat(",sigma12=",sigma12)
+sigma21 = as.numeric(args[5]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",sigma21=",sigma21)
+prop.immune = as.numeric(args[6]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",prop.immune=",prop.immune)
+nSeeds.1= as.numeric(args[7]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",nSeeds.1=",nSeeds.1)
+nSeeds.2= as.numeric(args[8]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",nSeeds.2=",nSeeds.2)
+rho.1= as.numeric(args[9]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",rho.1=",rho.1)
+rho.2= as.numeric(args[10]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",rho.2=",rho.2)
+alpha.as.1= as.numeric(args[11]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",alpha.as.1=",alpha.as.1)
+alpha.as.2= as.numeric(args[12]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",alpha.as.2=",alpha.as.2)
+R.1= as.numeric(args[13]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",R.1=",R.1)
+R.2= as.numeric(args[14]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",R.2=",R.2)
+IPL= as.numeric(args[15]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",IPL=",IPL)
+lli.k= as.numeric(args[16]) # short-term interaction parameter: acquiring 1 while having 2 (this value can)
+cat(",lli.k=",lli.k)
+
+
+
+#Input parameters - fixed
+library(ergm)
+library(RGeode)
+load("HH_complete.RData")
+HH.network<-HH_complete
+lambda.h<-3.18 #(computed as the average number of links individuals have within households)
+Net<-"HH_Eva"
+
+#ERGM.EGO
+#Networks<-readRDS("~/Documents/Work/PhD/Co-infection/Script/sim_EgoHHcombined_basis_complete_n_10.rds")
+#HH.network<-Networks[[1]]
+#id<- HH.network %v% ".NetworkID"
+#set.vertex.attribute(HH.network, "hh_id", value = id)
+#n.link<-NULL
+#hh_size<-NULL
+#for (i in 1:length(id)) {
+#  n.link<-c(n.link,length(get.neighborhood(HH.network,i,type = "out")))
+#  hh_size<-c(hh_size, which(id==i))
+#}
+#set.vertex.attribute(HH.network, "hh_size", value = hh_size)
+#avg.ct<-mean(n.link)
+#lambda.h<-avg.ct
+
+
+lambda.g<-8.29
+
+#SettingInfectivityParameters
+# direct computation
+source("R_comp_netw.R")
+ratio_hhgl<-1.31/lambda.g*lambda.h
+R.rif<-R.1
+nSim<-100
+tol<-0.05
+nSeed<-3082021
+set.seed(nSeed)
+trs.prms<-R0.comp(ratio_hhgl=ratio_hhgl, HH.network = HH.network, nSim = nSim, tol=tol,R.rif = R.rif )
+
+#load data
+inf.path.1.h<-trs.prms$beta.h/lambda.h
+inf.path.1.g<-trs.prms$beta.g/lambda.g
+
+
+R.rif<-R.2
+trs.prms<-R0.comp(ratio_hhgl=ratio_hhgl, HH.network = HH.network, nSim = nSim, tol=tol,R.rif = R.rif )
+
+inf.path.2.h<-trs.prms$beta.h/lambda.h
+inf.path.2.g<-trs.prms$beta.g/lambda.g
+
+#Compute the reproduction number related to the selected network. 
+source("function.multipathogen.new.R")
+nSim<-2000
+epi.outbreak<-list()
+nSeed<-1062021
+set.seed(nSeed)
+
+nm<-paste("t2_",t2, "_sigma12_",sigma12,"_sigma21_",sigma21,"_qh1_",inf.path.1.h,"_qg1_",inf.path.1.g,"_qh2_",inf.path.2.h,"_qg2_",inf.path.2.g, "_rho1_",rho.1,"_rho2_",rho.2,"_alpha1_",alpha.as.1,"_alpha2_",alpha.as.2,"_IPL",IPL,"_lli.k",lli.k, sep = "")
+print(nm)
+
+for (i in 1:nSim){
+  print(i)
+  epi.outbreak[[i]]<-sim.multipathogen(HH.network = HH.network, t2=t2, lambda.g = lambda.g, sigma12 = sigma12, sigma21 = sigma21, prop.immune = prop.immune, nSeeds.1 = nSeeds.1, nSeeds.2 = nSeeds.2, rho.1 = rho.1, rho.2 = rho.2, inf.path.1.h = inf.path.1.h,inf.path.1.g = inf.path.1.g, inf.path.2.h = inf.path.2.h,inf.path.2.g = inf.path.2.g, alpha.as.1=alpha.as.1,alpha.as.2=alpha.as.2, IPL=IPL, lli.k=lli.k)
+  }
+
+scen<-"BSL"
+
+name<-paste("MP_",scen,"_R1",R.1,"_R2",R.2,"_t2",t2,"_sigma12_",sigma12,"_sigma21_",sigma21,"_alpha1",alpha.as.1,"_alpha2",alpha.as.2,"_rho1",rho.1,"_rho2",rho.2,"_IPL",IPL,"_lli",lli.k,"_Net",Net,"_.RData", sep = "")
+setwd(out)
+save(epi.outbreak, file = name)
+
