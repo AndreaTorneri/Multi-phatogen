38832978d17e3b90e15911edd300e77d77250a52
ATorneri <andrea.torneri@gmail.com>
2023-07-26 11:02:16 +0200

    TPStartingPoint

--- R_comp_netw.R
+++ R_comp_netw.R
@@ -150,143 +150,6 @@ avg.lambda.within<-function(HH.networks){
 }
 
 
-# R0.computation.Inf<-function(HH.network,beta.g,nSim, beta.h,prob.asym,asymp.rel.inf,lambda.h,pathogen,ctc.dec,compl){
-#   mu<-infectious.period.length(pathogen = pathogen)
-#   meanIP<-mean.ip(pathogen = pathogen)
-#   hh.id<-HH.network %v% "hh_id"
-#   hh.size<-HH.network %v% "hh_size"
-#   n<-length(hh.id)
-#   m<- length(unique(hh.id)) # number of households
-#   m.n<-rep(0,max(unique(hh.size))) # number of household of a specific size (position 1 size 1, position 2 size 2, ....)
-#   R0<-0
-#   for (i in 1:length(m.n)){
-#     m.n[i]<-length(unique(hh.id[which(hh.size==i)]))
-#   }  
-#   h.n<-m.n/m #proportion of household of a specific size
-#   mu.h<-sum(h.n*(1:length(h.n)))
-#   #divide network in sizes
-#   for (j in 1:nSim){
-#     AR<-list()
-#     for (s in 1:max(unique(hh.size))) {
-#       AR[[s]]<-0  
-#     }
-#     n.asympt<-0
-#     q.h<-beta.h/lambda.h
-#     for (w in unique(hh.id)){
-#       #print(w)
-#       hh.data<-data.frame("members"= which(hh.id==w),"id"=1:length(which(hh.id==w)),"status"=0,"recovery"=Inf, "index.contact"=0, "betah"=0, "SO"=Inf, "ToI"=Inf)
-#       #index case
-#       for (r in hh.data$id){
-#         ifelse(length(get.neighborhood(HH.network, hh.data$members[r]))>0,hh.data$betah[r]<-(length(get.neighborhood(HH.network, hh.data$members[r]))*q.h),hh.data$betah[r]<-1/rexp(1,1/exp(100))) 
-#         if (runif(1)<prob.asym){
-#           hh.data$betah[r]<-hh.data$betah[r]*asymp.rel.inf
-#           n.asympt<-n.asympt+1
-#         }
-#       }
-#       primary<-sample(1:length(hh.data$members),1)
-#       hh.data$status[primary]<-1 
-#       hh.data$recovery[primary]<-mu
-#       hh.data$index.contact[primary]<-1
-#       hh.data$SO[primary]<-incubation.period(pathogen = pathogen)
-#       hh.data$ToI[primary]<-0
-#       contact.time<-data.frame("id"=hh.data$members,"pr.ctc"=rep(NA,length(hh.data$members)),"pr.infectee"=rep(NA,length(hh.data$members)))   #matrix containing the proposed time of the next possible infectious contact (first colum) 
-#       current.time<-0
-#       events<-data.frame(NextCtc        = Inf,
-#                          SymptOns       = Inf,
-#                          Recovery       = Inf)
-#       while ((length(which(hh.data$status==1))>0)) { # till there is at least one infectious individual
-#         for (i in which(hh.data$index.contact==1) ){ # for all the individuals that has to propose a new contact
-#           temp.contact.time<-rexp(1,hh.data$betah[i])+current.time
-#            hh.members.contacted<-get.neighborhood(HH.network,hh.data$members[i], type = "out")
-#           hh.data$index.contact[i]<-0
-#           ifelse(length(hh.members.contacted)>0,contact.time$pr.infectee[i] <-sample(hh.members.contacted,1),contact.time$pr.infectee[i] <-NA)
-#           if (length(hh.members.contacted)==1){contact.time$pr.infectee[i]<-hh.members.contacted}
-#           ifelse(length(hh.members.contacted)>0,contact.time$pr.ctc[i]<-temp.contact.time ,contact.time$pr.ctc[i]<-NA)
-#         }
-#         #computation of the next event
-#         ifelse(length(which(is.na(contact.time$pr.ctc)==FALSE))>0,events$NextCtc<-min(contact.time$pr.ctc, na.rm = T),events$NextCtc<-Inf)
-#         events$Recovery<-min(hh.data$recovery, na.rm = T)
-#         events$SymptOns<-min(hh.data$SO, na.rm = T)
-#         
-#         next.evts<-colnames(events)[min(events)==events]
-#         if (length(next.evts)>1){
-#           next.evts<-sample(next.evts,1)
-#         }
-#         #next event is an infection
-#         
-#         if (next.evts=="NextCtc"){
-#           current.time<-events$NextCtc
-#           infector<-which(contact.time$pr.ctc ==current.time)
-#           infectee<-hh.data$id[which(hh.data$members==contact.time$pr.infectee[infector])]
-#           if (hh.data$status[infectee]==0 & runif(1)<InfMeasure(t=current.time-hh.data$ToI[infector], pathogen = pathogen)){
-#             hh.data$status[infectee]<-1
-#             hh.data$recovery[infectee]<-current.time+mu
-#             hh.data$SO[infectee]<-current.time+incubation.period(pathogen = pathogen)
-#             hh.data$ToI[infectee]<-current.time
-#             hh.data$index.contact[infectee]<-1
-#             hh.data$index.contact[infector]<-1
-#             contact.time[infector,2:3]<-NA
-#           }else{
-#             hh.data$index.contact[infector]<-1
-#             contact.time[infector,2:3]<-NA
-#           }
-#           
-#         }
-#         if (next.evts=="Recovery"){
-#           current.time<-events$Recovery
-#           recovered<-which(hh.data$recovery==current.time)
-#           hh.data$recovery[recovered]<-Inf
-#           hh.data$status[recovered]<--1
-#           contact.time[recovered,2:3]<-rep(NA,2)
-#         }
-#         if (next.evts=="SymptOns"){
-#           current.time<-events$SymptOns
-#           symptomatic<-which(hh.data$SO==current.time)
-#           hh.data$SO[symptomatic]<-Inf
-#           contact.time[symptomatic,2:3]<-rep(NA,2)
-#           if (ctc.dec!=0){
-#           hh.data$betah[symptomatic]<-hh.data$betah[symptomatic]*ctc.dec
-#           hh.data$index.contact[symptomatic]<-1
-#           }
-#         }
-#   
-#       }
-#       AR[[hh.size[hh.data$members[1]]]]<-c(AR[[hh.size[hh.data$members[1]]]],length(which(hh.data$status==-1)))
-#       # sar - NA when no symptomatic infection are register
-#     }
-#     
-#     ar<-0
-#     for (s in 1:max(unique(hh.size))){
-#       ar[s]<-ifelse(length(AR[[s]])>1,mean(AR[[s]][-1]),0) 
-#     }
-#     
-#     if (compl==0){
-#       if (n.asympt==n){
-#         R0[j]<-(beta.g*mu*asymp.rel.inf)*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#       }
-#       if (n.asympt==0){
-#         R0[j]<-(beta.g*mu)*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#       }
-#       if (n.asympt!=n & n.asympt!=0){
-#         R0[j]<-((beta.g*mu*(n- n.asympt)/n)+ (beta.g*mu*asymp.rel.inf*n.asympt/n))*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#       }  
-#       
-#     }else{
-#       if (n.asympt==n){
-#         R0[j]<-(beta.g*asymp.rel.inf)*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#       }
-#       if (n.asympt==0){
-#         R0[j]<-(beta.g*meanIP*compl+beta.g*mu*(1-compl)) *(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#       }
-#       if (n.asympt!=n & n.asympt!=0){
-#         R0[j]<-(((beta.g*meanIP*compl+beta.g*mu*(1-compl))*(n- n.asympt)/n)+ (beta.g*mu*asymp.rel.inf*n.asympt/n))*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#        # R0[j]<-(((beta.g*compl/0.2885224+beta.g*mu*(1-compl))*(n- n.asympt)/n)+ (beta.g*mu*asymp.rel.inf*n.asympt/n))*(sum(ar*(h.n)*(1:max(unique(hh.size)))))/mu.h
-#       }  
-#     }
-#     print(j)
-#   }
-#   return(R0)
-# }
 
 R0.computation.Inf.new<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf,lambda.g,pathogen,ctc.dec,compl){
   mu<-infectious.period.length(pathogen = pathogen)
@@ -468,7 +331,7 @@ R0.comp.Inf.new<-function(ratio_hhgl,tol,R.rif,HH.network,nSim,prob.asym,asymp.r
 
 R0.comp.Inf.bc<-function(ratio_hhgl,tol,R.rif,HH.network,nSim,prob.asym,asymp.rel.inf,lambda.g,pathogen,ctc.dec,compl){
   mu<-infectious.period.length(pathogen = pathogen)
-  q.g<-0.2 # initial point
+  q.g<-0.1 # initial point
   q.h<-ratio_hhgl*q.g
   q.g.tempm<-0
   q.g.tempM<-1
@@ -532,20 +395,17 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
         #index case
         for (r in hh.data$id){
           ifelse(length(get.neighborhood(HH.network, hh.data$members[r]))>0,hh.data$betah[r]<-(length(get.neighborhood(HH.network, hh.data$members[r]))),hh.data$betah[r]<-1/rexp(1,1/exp(100))) 
-          if (runif(1)<prob.asym){
-            hh.data$betah[r]<-hh.data$IM[r]*asymp.rel.inf
-          }
         }
         primary<-sample(1:length(hh.data$members),1)
         hh.data$status[primary]<-1 
         hh.data$recovery[primary]<-mu
         hh.data$index.contact[primary]<-1
-        hh.data$SO[primary]<-incubation.period(pathogen = pathogen)
+        hh.data$IM[primary]<-hh.data$IM[primary]*asymp.rel.inf
         hh.data$ToI[primary]<-0
         contact.time<-data.frame("id"=hh.data$members,"pr.ctc"=rep(NA,length(hh.data$members)),"pr.infectee"=rep(NA,length(hh.data$members)))   #matrix containing the proposed time of the next possible infectious contact (first colum) 
         current.time<-0
         events<-data.frame(NextCtc        = Inf,
-                           SymptOns       = Inf,
+                           HomeIso       = Inf,
                            Recovery       = Inf)
         while ((length(which(hh.data$status==1))>0)) { # till there is at least one infectious individual
           for (i in which(hh.data$index.contact==1) ){ # for all the individuals that has to propose a new contact
@@ -574,7 +434,13 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
             if (hh.data$status[infectee]==0 & runif(1)<(InfMeasure(t=current.time-hh.data$ToI[infector], pathogen = pathogen)*hh.data$IM[infector])){
               hh.data$status[infectee]<-1
               hh.data$recovery[infectee]<-current.time+mu
-              hh.data$SO[infectee]<-current.time+incubation.period(pathogen = pathogen)
+              if (runif(1)<prob.asym){
+                hh.data$IM[infectee]<-hh.data$IM[infectee]*asymp.rel.inf
+              }else{
+                if(runif(1)<compl){
+                  hh.data$SO[infectee]<-current.time+incubation.period(pathogen = pathogen)
+                }
+              }
               hh.data$ToI[infectee]<-current.time
               hh.data$index.contact[infectee]<-1
               hh.data$index.contact[infector]<-1
@@ -583,7 +449,6 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
               hh.data$index.contact[infector]<-1
               contact.time[infector,2:3]<-NA
             }
-            
           }
           if (next.evts=="Recovery"){
             current.time<-events$Recovery
@@ -592,15 +457,15 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
             hh.data$status[recovered]<--1
             contact.time[recovered,2:3]<-rep(NA,2)
           }
-          if (next.evts=="SymptOns"){
-            current.time<-events$SymptOns
+          if (next.evts=="HomeIso"){
+            current.time<-events$SHomeIso
             symptomatic<-which(hh.data$SO==current.time)
             hh.data$SO[symptomatic]<-Inf
             contact.time[symptomatic,2:3]<-rep(NA,2)
-            if (ctc.dec!=0){
-              hh.data$betah[symptomatic]<-hh.data$betah[symptomatic]*ctc.dec
-              hh.data$index.contact[symptomatic]<-1
-            }
+              if (ctc.dec!=0){
+                hh.data$betah[symptomatic]<-hh.data$betah[symptomatic]*ctc.dec
+                hh.data$index.contact[symptomatic]<-1
+              }
           }
           
         }
@@ -624,20 +489,20 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
         #index case
         for (r in hh.data$id){
           ifelse(length(get.neighborhood(HH.network, hh.data$members[r]))>0,hh.data$betah[r]<-(length(get.neighborhood(HH.network, hh.data$members[r]))),hh.data$betah[r]<-1/rexp(1,1/exp(100))) 
-          if (runif(1)<prob.asym){
-            hh.data$betah[r]<-hh.data$IM[r]*asymp.rel.inf
-          }
         }
         primary<-sample(1:length(hh.data$members),1)
         hh.data$status[primary]<-1 
         hh.data$recovery[primary]<-mu
         hh.data$index.contact[primary]<-1
-        hh.data$SO[primary]<-incubation.period(pathogen = pathogen)
+        if(runif(1)<compl){
+          hh.data$SO[primary]<-current.time+incubation.period(pathogen = pathogen)
+        }
+        hh.data$IM[primary]<-hh.data$IM[primary]*asymp.rel.inf
         hh.data$ToI[primary]<-0
         contact.time<-data.frame("id"=hh.data$members,"pr.ctc"=rep(NA,length(hh.data$members)),"pr.infectee"=rep(NA,length(hh.data$members)))   #matrix containing the proposed time of the next possible infectious contact (first colum) 
         current.time<-0
         events<-data.frame(NextCtc        = Inf,
-                           SymptOns       = Inf,
+                           HomeIso       = Inf,
                            Recovery       = Inf)
         while ((length(which(hh.data$status==1))>0)) { # till there is at least one infectious individual
           for (i in which(hh.data$index.contact==1) ){ # for all the individuals that has to propose a new contact
@@ -666,7 +531,13 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
             if (hh.data$status[infectee]==0 & runif(1)<(InfMeasure(t=current.time-hh.data$ToI[infector], pathogen = pathogen)*hh.data$IM[infector])){
               hh.data$status[infectee]<-1
               hh.data$recovery[infectee]<-current.time+mu
-              hh.data$SO[infectee]<-current.time+incubation.period(pathogen = pathogen)
+              if (runif(1)<prob.asym){
+                hh.data$IM[infectee]<-hh.data$IM[infectee]*asymp.rel.inf
+              }else{
+                if(runif(1)<compl){
+                  hh.data$SO[infectee]<-current.time+incubation.period(pathogen = pathogen)
+                }
+              }
               hh.data$ToI[infectee]<-current.time
               hh.data$index.contact[infectee]<-1
               hh.data$index.contact[infector]<-1
@@ -675,7 +546,6 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
               hh.data$index.contact[infector]<-1
               contact.time[infector,2:3]<-NA
             }
-            
           }
           if (next.evts=="Recovery"){
             current.time<-events$Recovery
@@ -684,8 +554,8 @@ R0.computation.Inf.bc<-function(HH.network,q.g,nSim, q.h,prob.asym,asymp.rel.inf
             hh.data$status[recovered]<--1
             contact.time[recovered,2:3]<-rep(NA,2)
           }
-          if (next.evts=="SymptOns"){
-            current.time<-events$SymptOns
+          if (next.evts=="HomeIso"){
+            current.time<-events$SHomeIso
             symptomatic<-which(hh.data$SO==current.time)
             hh.data$SO[symptomatic]<-Inf
             contact.time[symptomatic,2:3]<-rep(NA,2)
