54fe9f435fa606f698b39c42ad791b0fdc96ae7d
nadinebarth <nadine.barth@uhasselt.be>
2024-12-20 09:40:01 +0100

    Update and bug fix in creating synthetic networks
    The script for creating synthetic networks had a bug when the network did not contain all household sizes (especially problematic when creating a small household for testing purposes). This has been fixed.
    Also, the script has been changed to be more general, the maximal network size (now set to 7) can be edited in the beginning of the script and the remainder of the script adapts accordingly.
    Comments are added.

--- CreatingSynthNetw.R
+++ CreatingSynthNetw.R
@@ -1,198 +1,89 @@
 
-creat.synth.netw<-function(n.vertex,n.networks,density_by_hh_size){
-  # Computing the ratio q_h/q_g
+##################################################
+### Function to create a new synthetic network ###
+### Called by the script 'main.SynthNetw.R'    ###
+##################################################
+
+## This function generates a given number (n.networks) of new synthetic networks 
+## with a given number of households (n.vertex) based on a given distribution of 
+## household sizes (density_by_hh_size). 
+
+create.synth.netw <- function(n.vertex, n.networks, density_by_hh_size){
+  # load the needed library
   library(network)
   
-  HH.netw<-list()
-  n<-n.vertex
-  # Compute the synthetic scenarios
-  max.hhsz<-7
-  data.hhnetw<-density_by_hh_size[1:max.hhsz,]
-  prob.hhsize<-data.hhnetw$household_size_freq/sum(data.hhnetw$household_size_freq)
-  count<-0
-  hh.size.synth<-data.frame("Size"=1:max.hhsz,"Freq"=0)
+  # Create an empty list to store the networks
+  HH.netw <- list()
+  # determine a maximal household size according to this maximal household size
+  max.hhsz <- 7 
+  # extract the relevant rows from the household density data set and compute the relative proportions
+  data.hhnetw <- density_by_hh_size[1:max.hhsz,]
+  prob.hhsize <- data.hhnetw$household_size_freq/sum(data.hhnetw$household_size_freq)
   
-  for (w in 1:n.networks){
-    while(count<n){
-      k<-sample(1:max.hhsz,1,prob = prob.hhsize)
-      hh.size.synth$Freq[which(hh.size.synth$Size==k)]<-hh.size.synth$Freq[which(hh.size.synth$Size==k)]+1
-      count<-count+k
+  # this count is needed to keep track of how large the network is during the creation process
+  count <- 0
+  
+  # create a data frame to store the frequency of households of each size
+  # the first column contains the household sizes (from 1 to max.hhsz) and the second column the frequency
+  hh.size.synth <- data.frame("Size" = 1:max.hhsz, "Freq" = 0)
+  
+  ## sample household sizes between 1 and max.hhsz with according probabilities and keep track of the numbers 
+  ## of households of specific sizes in hh.size.synth
+  for (w in 1:n.networks){ # for each network to create
+    # create households of different sizes until the network is of desired size. 
+    while(count<n.vertex){ # check whether the total population size is not yet exceeded
+      # k is the household size (sampled)
+      k <- sample(1:max.hhsz,1,prob = prob.hhsize)
+      # update the data.frame that keeps track of frequency of networks of different sizes
+      hh.size.synth$Freq[which(hh.size.synth$Size==k)] <- hh.size.synth$Freq[which(hh.size.synth$Size==k)]+1
+      # update the count
+      count <- count+k
     }
     
-    adj.mat<-matrix(data=0,nrow = count, ncol = count)
-    hh.size.vect<-rep(1,count)
-    temp21<-(hh.size.synth$Freq[1]+1)
-    temp22<-hh.size.synth$Freq[1]+2*hh.size.synth$Freq[2]
-    for(j in seq(temp21,temp22,by=2) ){
-      adj.mat[j,j+1]<-1
-      adj.mat[j+1,j]<-1
-      hh.size.vect[j:(j+1)]<-2
+    # create a square matrix of size nxn to be the adjacency matrix of network members
+    adj.mat <- matrix(data=0,nrow = count, ncol = count)
+    # create a vector of length n
+    hh.size.vect <- rep(1,count)
+    
+    ## fill in hh.size.vect
+    temp2 <- hh.size.synth$Freq[1] * (1)
+    for(i in 2:max.hhsz){ # for all household sizes starting from size 2
+      if(hh.size.synth[i,2]!=0){ # extra check for the case that a certain household size is not represented
+        temp1 = temp2
+        temp2 = hh.size.synth$Freq[i] * (i) + temp1
+        hh.size.vect[(temp1+1):(temp2)] = i
+        # update the adjacency matrix
+        for(j in seq(temp1+1, temp2, by = i)){
+          household = seq(j,j+i-1,1)
+          combinations = combn(household,2)
+          for(x in 1:ncol(combinations)){
+            adj.mat[combinations[1,x], combinations[2,x]] = i
+            adj.mat[combinations[2,x], combinations[1,x]] = i
+          }
+        }
+      }
     }
     
-    temp31<-temp22+1
-    temp32<-hh.size.synth$Freq[1]+2*hh.size.synth$Freq[2]+3*hh.size.synth$Freq[3]
-    for(j in seq(temp31,temp32,by=3) ){
-      adj.mat[j,j+1]<-1
-      adj.mat[j,j+2]<-1
-      adj.mat[j+1,j]<-1
-      adj.mat[j+1,j+2]<-1
-      adj.mat[j+2,j]<-1
-      adj.mat[j+2,j+1]<-1
-      hh.size.vect[seq(j,j+2,1)]<-3
+    # Constructing hh.id, a vector that contains the household size of each individual at its index. 
+    hh.id.vect <- c(1:hh.size.synth$Freq[1]) # fill vector for household size = 1
+    for(i in 2:max.hhsz){ # idem for household sizes > 1
+      if(hh.size.synth[i,2] != 0){
+        temp.seq <- seq(hh.id.vect[length(hh.id.vect)]+1, hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[i]) # for all households of size 2
+        hh.id.vect <- c(hh.id.vect, rep(temp.seq,each = i))
+      }
     }
     
-    temp41<-temp32+1
-    temp42<-hh.size.synth$Freq[1]+2*hh.size.synth$Freq[2]+3*hh.size.synth$Freq[3]+4*hh.size.synth$Freq[4]
-    for(j in seq(temp41,temp42,by=4) ){
-      adj.mat[j,j+1]<-1
-      adj.mat[j,j+2]<-1
-      adj.mat[j,j+3]<-1
-      adj.mat[j+1,j]<-1
-      adj.mat[j+1,j+2]<-1
-      adj.mat[j+1,j+3]<-1
-      adj.mat[j+2,j]<-1
-      adj.mat[j+2,j+1]<-1
-      adj.mat[j+2,j+3]<-1
-      adj.mat[j+3,j+1]<-1
-      adj.mat[j+3,j+2]<-1
-      adj.mat[j+3,j]<-1
-      hh.size.vect[j:(j+3)]<-4
-    }
-    
-    temp51<-temp42+1
-    temp52<-hh.size.synth$Freq[1]+2*hh.size.synth$Freq[2]+3*hh.size.synth$Freq[3]+4*hh.size.synth$Freq[4]+5*hh.size.synth$Freq[5]
-    for(j in seq(temp51,temp52,by=5) ){
-      adj.mat[j,j+1]<-1
-      adj.mat[j,j+2]<-1
-      adj.mat[j,j+3]<-1
-      adj.mat[j,j+4]<-1
-      adj.mat[j+1,j]<-1
-      adj.mat[j+1,j+2]<-1
-      adj.mat[j+1,j+3]<-1
-      adj.mat[j+1,j+4]<-1
-      adj.mat[j+2,j+1]<-1
-      adj.mat[j+2,j]<-1
-      adj.mat[j+2,j+3]<-1
-      adj.mat[j+2,j+4]<-1
-      adj.mat[j+3,j+1]<-1
-      adj.mat[j+3,j+2]<-1
-      adj.mat[j+3,j]<-1
-      adj.mat[j+3,j+4]<-1
-      adj.mat[j+4,j+1]<-1
-      adj.mat[j+4,j+2]<-1
-      adj.mat[j+4,j+3]<-1
-      adj.mat[j+4,j]<-1
-      hh.size.vect[j:(j+4)]<-5
-    }
-    
-    temp61<-temp52+1
-    temp62<-hh.size.synth$Freq[1]+2*hh.size.synth$Freq[2]+3*hh.size.synth$Freq[3]+4*hh.size.synth$Freq[4]+5*hh.size.synth$Freq[5]+6*hh.size.synth$Freq[6]
-    for(j in seq(temp61,temp62,by=6) ){
-      adj.mat[j,j+1]<-1
-      adj.mat[j,j+2]<-1
-      adj.mat[j,j+3]<-1
-      adj.mat[j,j+4]<-1
-      adj.mat[j,j+5]<-1
-      adj.mat[j+1,j]<-1
-      adj.mat[j+1,j+2]<-1
-      adj.mat[j+1,j+3]<-1
-      adj.mat[j+1,j+4]<-1
-      adj.mat[j+1,j+5]<-1
-      adj.mat[j+2,j+1]<-1
-      adj.mat[j+2,j]<-1
-      adj.mat[j+2,j+3]<-1
-      adj.mat[j+2,j+4]<-1
-      adj.mat[j+2,j+5]<-1
-      adj.mat[j+3,j+1]<-1
-      adj.mat[j+3,j+2]<-1
-      adj.mat[j+3,j]<-1
-      adj.mat[j+3,j+4]<-1
-      adj.mat[j+3,j+5]<-1
-      adj.mat[j+4,j+1]<-1
-      adj.mat[j+4,j+2]<-1
-      adj.mat[j+4,j+3]<-1
-      adj.mat[j+4,j]<-1
-      adj.mat[j+4,j+5]<-1
-      adj.mat[j+5,j+1]<-1
-      adj.mat[j+5,j+2]<-1
-      adj.mat[j+5,j+3]<-1
-      adj.mat[j+5,j+4]<-1
-      adj.mat[j+5,j]<-1
-      hh.size.vect[j:(j+5)]<-6
-    }
-    
-    temp71<-temp62+1
-    temp72<-hh.size.synth$Freq[1]+2*hh.size.synth$Freq[2]+3*hh.size.synth$Freq[3]+4*hh.size.synth$Freq[4]+5*hh.size.synth$Freq[5]+6*hh.size.synth$Freq[6]+7*hh.size.synth$Freq[7]
-    for(j in seq(temp71,temp72,by=7) ){
-      adj.mat[j,j+1]<-1
-      adj.mat[j,j+2]<-1
-      adj.mat[j,j+3]<-1
-      adj.mat[j,j+4]<-1
-      adj.mat[j,j+5]<-1
-      adj.mat[j,j+6]<-1
-      adj.mat[j+1,j]<-1
-      adj.mat[j+1,j+2]<-1
-      adj.mat[j+1,j+3]<-1
-      adj.mat[j+1,j+4]<-1
-      adj.mat[j+1,j+5]<-1
-      adj.mat[j+1,j+6]<-1
-      adj.mat[j+2,j+1]<-1
-      adj.mat[j+2,j]<-1
-      adj.mat[j+2,j+3]<-1
-      adj.mat[j+2,j+4]<-1
-      adj.mat[j+2,j+5]<-1
-      adj.mat[j+2,j+6]<-1
-      adj.mat[j+3,j+1]<-1
-      adj.mat[j+3,j+2]<-1
-      adj.mat[j+3,j]<-1
-      adj.mat[j+3,j+4]<-1
-      adj.mat[j+3,j+5]<-1
-      adj.mat[j+3,j+6]<-1
-      adj.mat[j+4,j+1]<-1
-      adj.mat[j+4,j+2]<-1
-      adj.mat[j+4,j+3]<-1
-      adj.mat[j+4,j]<-1
-      adj.mat[j+4,j+5]<-1
-      adj.mat[j+4,j+6]<-1
-      adj.mat[j+5,j+1]<-1
-      adj.mat[j+5,j+2]<-1
-      adj.mat[j+5,j+3]<-1
-      adj.mat[j+5,j+4]<-1
-      adj.mat[j+5,j]<-1
-      adj.mat[j+5,j+6]<-1
-      adj.mat[j+6,j+1]<-1
-      adj.mat[j+6,j+2]<-1
-      adj.mat[j+6,j+3]<-1
-      adj.mat[j+6,j+4]<-1
-      adj.mat[j+6,j+5]<-1
-      adj.mat[j+6,j]<-1
-      hh.size.vect[j:(j+6)]<-7
-    }
-    
-    #Constructing hh.id
-    hh.id.vect<-NULL
-    hh.id.vect<-c(1:hh.size.synth$Freq[1])
-    temp.seq<-seq(hh.id.vect[length(hh.id.vect)]+1,hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[2])
-    hh.id.vect<-c(hh.id.vect,rep(temp.seq,each=2))
-    temp.seq<-seq(hh.id.vect[length(hh.id.vect)]+1,hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[3])
-    hh.id.vect<-c(hh.id.vect,rep(temp.seq,each=3))
-    temp.seq<-seq(hh.id.vect[length(hh.id.vect)]+1,hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[4])
-    hh.id.vect<-c(hh.id.vect,rep(temp.seq,each=4))
-    temp.seq<-seq(hh.id.vect[length(hh.id.vect)]+1,hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[5])
-    hh.id.vect<-c(hh.id.vect,rep(temp.seq,each=5))
-    temp.seq<-seq(hh.id.vect[length(hh.id.vect)]+1,hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[6])
-    hh.id.vect<-c(hh.id.vect,rep(temp.seq,each=6))
-    temp.seq<-seq(hh.id.vect[length(hh.id.vect)]+1,hh.id.vect[length(hh.id.vect)]+hh.size.synth$Freq[7])
-    hh.id.vect<-c(hh.id.vect,rep(temp.seq,each=7))
     vert.attr<-list()
     vert.attr.name<-list()
     vert.attr[[1]]<-hh.size.vect
     vert.attr[[2]]<-hh.id.vect
     vert.attr.name[[1]]<-"hh_size"
     vert.attr.name[[2]]<-"hh_id"
-    HH.netw[[w]]<-network(adj.mat, directed = FALSE, vertex.attr = vert.attr, vertex.attrnames =vert.attr.name )
+    HH.netw[[w]]<-network(adj.mat, 
+                          directed = FALSE, 
+                          vertex.attr = vert.attr, 
+                          vertex.attrnames = vert.attr.name)
   }
-  
   return(HH.netw)
 }
 
--- main.SynthNetw.R
+++ main.SynthNetw.R
@@ -1,16 +1,39 @@
+###########################################################################
+### This script creates a list of synthetic networks                   ####
+### It relies on functions defined in the script 'CreatingSynthNetw.R' ####
+###########################################################################
+
+### These parameters come from an external file 'CreatingNetw.csv' 
 args <- commandArgs(trailingOnly = TRUE)
-out = args[1] #working directory
+# working directory
+out <- args[1] 
 cat(",out=",out)
-cores = as.numeric(args[2]) #number of cores to run in parallel
+# number of cores to run in parallel
+cores <- as.numeric(args[2]) 
 cat(",cores=",cores)
-n.vertex = as.numeric(args[3]) #time at which pathogen 2 is insert in the population
+# number of individuals in the network
+n.vertex <- as.numeric(args[3]) 
 cat(",n.vertex=",n.vertex)
-n.networks = as.numeric(args[4]) #time at which pathogen 2 is insert in the population
+# numbers of networks to create
+n.networks <- as.numeric(args[4]) 
 cat(",n.networks=",n.networks)
 
+### For testing purposes, create a small network with the following parameters:
+n.vertex <- 100
+n.networks <- 1
+
+### Load necessary files
+## This script relies on functions defined in the script 'CreatingSynthNetw.R'
 source("CreatingSynthNetw.R")
+## Dataset containing densities for all household sizes. 
+# Set working directory if necessary
+# setwd("C:/Users/LUCP13441/Documents/GitHub/Multi-phatogen")
 load("DensityHHsize.RData")
 
-HH.networks<-creat.synth.netw(n.vertex = n.vertex,n.networks = n.networks,density_by_hh_size = density_by_hh_size)
-name<-paste("HH_Networks","_nVertex",n.vertex,"_nNetw",n.networks,".RData",sep = "")
-save(HH.networks,file = name)
+# Call the function to create a new list of networks
+HH.networks <- create.synth.netw(n.vertex = n.vertex,
+                                 n.networks = n.networks,
+                                 density_by_hh_size = density_by_hh_size)
+# Determine the name of the file and save it
+name <- paste("HH_Networks", "_nVertex", n.vertex, "_nNetw", n.networks, ".RData", sep = "")
+save(HH.networks, file = name)
